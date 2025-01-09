615a45c4d1dc1f48e36038fc11fba5a74de7fe04
202102187e <202102187e@utea.edu.pe>
2025-01-09 09:29:51 -0500

    QUIZZAPP FINAL

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:app05_quizapp_20242/question.dart';
+
+import 'package:app_xylofonx/quiz_brain.dart';
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() => runApp(const MyApp());
 
@@ -22,30 +24,49 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  int questionNumber = 0;
-
-  List<String> questions = [
-    "El hombre llegó a la Luna?",
-    "Tomaste desayuno?",
-    "El tomaylla es arquero?",
-    "Faltas a clases?",
-  ];
-
-  List<bool> answers = [
-    true,
-    true,
-    false,
-    true,
-  ];
-
   List<Widget> scoreKeeper = [];
 
-  List<Question> questions1 = [
-    Question(questionText: "El hombre llegó a la Luna?", questionAnswer: true),
-    Question(questionText: "Tomaste desayuno?", questionAnswer: true),
-    Question(questionText: "El tomaylla es arquero?", questionAnswer: false),
-    Question(questionText: "Faltas a clases?", questionAnswer: true),
-  ];
+  QuizBrain quizBrain = QuizBrain();
+
+  checkAnswer(bool userAnswer) {
+    if (quizBrain.isFinished() == true) {
+      Alert(
+        context: context,
+        type: AlertType.success,
+        title: "QuizApp",
+        desc: "El quiz ha finalizado.",
+        buttons: [
+          DialogButton(
+            child: Text(
+              "Aceptar",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            onPressed: () => Navigator.pop(context),
+            width: 120,
+          )
+        ],
+      ).show();
+      //mensaje finalizado
+      quizBrain.restart();
+      scoreKeeper.clear();
+      setState(() {});
+    } else {
+      bool correctAnswer = quizBrain.getQuestionAnswer();
+
+      if (correctAnswer == userAnswer) {
+        scoreKeeper.add(
+          Icon(Icons.check, color: Colors.greenAccent),
+        );
+      } else {
+        scoreKeeper.add(
+          Icon(Icons.close, color: Colors.redAccent),
+        );
+      }
+
+      quizBrain.nextQuestion();
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,7 +87,8 @@ class _QuizPageState extends State<QuizPage> {
             flex: 5,
             child: Center(
               child: Text(
-                questions1[questionNumber].questionText,
+                //quizBrain.questions[questionNumber].questionText,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: const TextStyle(fontSize: 20, color: Colors.white),
               ),
@@ -78,21 +100,7 @@ class _QuizPageState extends State<QuizPage> {
               padding: const EdgeInsets.all(8.0),
               child: MaterialButton(
                 onPressed: () {
-                  bool correctAnswer =
-                      questions1[questionNumber].questionAnswer;
-
-                  if (correctAnswer == true) {
-                    scoreKeeper.add(
-                      Icon(Icons.check, color: Colors.greenAccent),
-                    );
-                  } else {
-                    scoreKeeper.add(
-                      Icon(Icons.close, color: Colors.redAccent),
-                    );
-                  }
-
-                  questionNumber++;
-                  setState(() {});
+                  checkAnswer(true);
                 },
                 color: Colors.greenAccent,
                 child: const Text("Verdadero"),
@@ -105,20 +113,7 @@ class _QuizPageState extends State<QuizPage> {
               padding: const EdgeInsets.all(8.0),
               child: MaterialButton(
                 onPressed: () {
-                  bool correctAnswer =
-                      questions1[questionNumber].questionAnswer;
-
-                  if (correctAnswer == false) {
-                    scoreKeeper.add(
-                      Icon(Icons.check, color: Colors.greenAccent),
-                    );
-                  } else {
-                    scoreKeeper.add(
-                      Icon(Icons.close, color: Colors.redAccent),
-                    );
-                  }
-                  questionNumber++;
-                  setState(() {});
+                  checkAnswer(false);
                 },
                 color: Colors.redAccent,
                 child: const Text("Falso"),
---
+++ lib/quiz_brain.dart
@@ -0,0 +1,39 @@
+
+import 'package:app_xylofonx/question.dart';
+
+class QuizBrain {
+  List<Question> _questions = [
+    Question(questionText: "El hombre llegó a la Luna?", questionAnswer: true),
+    Question(questionText: "Tomaste desayuno?", questionAnswer: true),
+    Question(questionText: "El tomaylla es arquero?", questionAnswer: false),
+    Question(questionText: "Faltas a clases?", questionAnswer: true),
+  ];
+
+  int _questionNumber = 0;
+
+  getQuestionText() {
+    return _questions[_questionNumber].questionText;
+  }
+
+  getQuestionAnswer() {
+    return _questions[_questionNumber].questionAnswer;
+  }
+
+  nextQuestion() {
+    if (_questionNumber < _questions.length - 1) {
+      _questionNumber++;
+    }
+  }
+
+  bool isFinished() {
+    if (_questionNumber >= _questions.length - 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void restart() {
+    _questionNumber = 0;
+  }
+}
